97a54574b538575edff454c6e8a3ad20bfc23070
damaris <damarisdallorto@outlook.com>
2022-07-13 10:51:56 -0500

    Updates

---
+++ precache-manifest.f5c7226e6c6c83c566332a7000535f0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b146122997c0697628a2",
+    "url": "/Hotel-Reservation-Project/static/css/main.50908b45.chunk.css"
+  },
+  {
+    "revision": "b146122997c0697628a2",
+    "url": "/Hotel-Reservation-Project/static/js/main.b1461229.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/Hotel-Reservation-Project/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "baf3456ae29d7457a266",
+    "url": "/Hotel-Reservation-Project/static/js/2.baf3456a.chunk.js"
+  },
+  {
+    "revision": "8569cbbead34a21491c1",
+    "url": "/Hotel-Reservation-Project/static/js/runtime~main.8569cbbe.js"
+  },
+  {
+    "revision": "02b36273c9dda4b1f4d7f3b2237ef1fa",
+    "url": "/Hotel-Reservation-Project/index.html"
+  }
+];
